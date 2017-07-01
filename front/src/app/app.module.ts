669be7229f1f581e909791bd96b07e39b97ee6b5
KseniyaRomanchik <ks.romanchik@gmail.com>
2017-07-01 16:45:15 +0300

    add front routing

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,13 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
+import { appRoutes } from './routes';
+
 import { AppComponent } from './app.component';
+import { MainComponent } from './routes/main/main.component';
+import { GameComponent } from './routes/game/game.component';
+import { GamesComponent } from './routes/games/games.component';
+import { Page404Component } from './routes/page404/page404.component';
+import { HeaderComponent } from './elements/header/header.component';
+import { SidebarComponent } from './routes/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    GameComponent,
+    GamesComponent,
+    Page404Component,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { 
+        enableTracing: true 
+      }
+    ),
     BrowserModule
   ],
   providers: [],
